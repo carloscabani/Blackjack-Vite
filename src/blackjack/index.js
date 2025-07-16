4dc290bb96fbb3ad99ba3a3c5f47b1120325ee04
carloscabani <cacabani@espol.edu.ec>
2025-07-16 15:34:33 -0500

    refactor: move crearDeck function to separate module

--- src/blackjack/index.js
+++ src/blackjack/index.js
@@ -1,5 +1,5 @@
 import _ from 'underscore';
-import './style.css';
+import {crearDeck} from './usecases/crear-deck.js';
 
 /**
  * 2C = Two of Clubs
@@ -25,28 +25,7 @@ const divCartasComputadora = document.querySelector('#computadora-cartas');
 
 const puntosHTML = document.querySelectorAll('small');
 
-// Esta función crea un nuevo deck
-const crearDeck = () => {
-
-    for( let i = 2; i <= 10; i++ ) {
-        for( let tipo of tipos ) {
-            deck.push( i + tipo);
-        }
-    }
-
-    for( let tipo of tipos ) {
-        for( let esp of especiales ) {
-            deck.push( esp + tipo);
-        }
-    }
-    // console.log( deck );
-    deck = _.shuffle( deck );
-    console.log( deck );
-    return deck;
-}
-
 crearDeck();
-
 
 // Esta función me permite tomar una carta
 const pedirCarta = () => {
